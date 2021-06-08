10e8cee9e016741b6f863b9bf4786782952d8062
TonyVFullStack <tony.vincenti.tv@gmail.com>
2021-06-08 15:21:27 +0200

    Component Button + Children HTML

--- app/src/App/App.js
+++ app/src/App/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import Button from './components/Button/Button'
 function App() {
   return (
     <div className="App">
       Demat Breizh
+      <hr/>
+      <Button>
+        <img src="https://cdn1.iconfinder.com/data/icons/science-technology-outline/91/Science__Technology_23-256.png" alt ="click" />
+        Hello
+      </Button>
+      <Button title="Benjamin"></Button>
+      <Button title="Cliquez ici"></Button>
+      <Button title="Cliquez pas ici"></Button>
     </div>
   );
 }
---
+++ app/src/App/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Button.css';
+
+function Button(props) {
+    console.log(props);
+    return <button className='Button'>{props.children}</button>;
+}
+
+export default Button;
